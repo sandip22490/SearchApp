befdb37c9560a2fed5c3c32c25c057184b447233
Sandip Patel <SandipP@sfwltd.co.uk>
2019-10-16 19:25:44 +0530

    Initial Commit

---
+++ Hackathon/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Hackathon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Elasticsearch.Net;
+using Elasticsearch.Net.Aws;
+using Nest;
+using Amazon;
+using Amazon.Runtime;
+using System.Web.Configuration;
+
+namespace Hackathon.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        class DocumentAttributes
+        {
+            public string s3link { get; set; }
+        }
+
+        private HackathonDBEntities db = new HackathonDBEntities();
+        public ViewResult Index(string SearchString,string SearchString1,string SearchString2)
+        {
+            string esDomain = WebConfigurationManager.AppSettings["EsEndpoint"];
+
+            var credentials = new EnvironmentAWSCredentials();
+            var httpConnection = new AwsHttpConnection(credentials, RegionEndpoint.USEast1);
+
+            var pool = new SingleNodeConnectionPool(new Uri(esDomain));
+            var config = new ConnectionSettings(pool, httpConnection);
+            var client = new ElasticClient(config);
+
+            var getTableData = client.Search<DocumentAttributes>(s => s.Index("resume")); // you can get maximum 10000 records at a time  
+
+            List<DocumentAttributes> lstData = new List<DocumentAttributes>();
+
+            foreach (var hit in getTableData.Hits)
+            {
+                lstData.Add(hit.Source);
+            }
+           
+            return View(lstData);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Report()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hackathon/Controllers/UploadController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System.Web.Configuration;
+
+namespace Hackathon.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles()
+        {
+            // Checking no of files injected in Request object  
+            if (Request.Files.Count > 0)
+            {
+                try
+                {
+                    //  Get all files from Request object  
+                    HttpFileCollectionBase files = Request.Files;
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        //string path = AppDomain.CurrentDomain.BaseDirectory + "Uploads/";  
+                        //string filename = Path.GetFileName(Request.Files[i].FileName);  
+
+                        HttpPostedFileBase file = files[i];
+                        string fname = files[i].FileName;
+                        string ext = Path.GetExtension(fname);
+                        if (ext == ".doc" || ext == ".docx" || ext == ".pdf" && files[i].ContentLength > 0)
+                        {
+                            // Get the complete folder path and store the file inside it.  
+                            //fname = Path.Combine(Server.MapPath("~/Uploads/"), fname);
+                            //file.SaveAs(fname);
+                            // Returns message that successfully uploaded  
+
+                            string accessKey = WebConfigurationManager.AppSettings["AWSAccessKey"];
+                            string secreteKey = WebConfigurationManager.AppSettings["AWSSecretKey"];
+                            string s3BucketName = WebConfigurationManager.AppSettings["S3UploadBucket"];
+
+                            AmazonS3Client client = new AmazonS3Client(accessKey, secreteKey, RegionEndpoint.USEast1);
+
+                            PutObjectRequest putRequest = new PutObjectRequest
+                            {
+                                BucketName = s3BucketName,
+                                Key = fname,
+                                InputStream = file.InputStream,
+                                ContentType = file.ContentType,
+                            };
+
+                            PutObjectResponse response = client.PutObject(putRequest);
+
+                            var result = new { IsSuccess = true, Message = "File Uploaded Successfully!" };
+                            return Json(result);
+                        }
+                        else
+                        {
+                            var result = new { IsSuccess = false, Message = "Currently this tool supports PDF..Coming soon!!" };
+                            return Json(result);
+                        }
+                    }
+                    return Json(" ");
+
+                }
+                catch (Exception ex)
+                {
+                    var result = new { IsSuccess = false, Message = ex.Message };
+                    return Json(result);
+                }
+            }
+            else
+            {
+                var result = new { IsSuccess = false, Message = "No files selected." };
+                return Json(result);
+            }
+        }
+    }
+}
